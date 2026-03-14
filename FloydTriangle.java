cacab36c797308e191e1c7f6a76467b412fe0240
Tanuja Kohale <kohaletanuja40@gmail.com>
2026-03-14 16:32:36 +0530

    Modify Floyd's Triangle to use fixed input
    Replaced user input with a fixed value for n and simplified output formatting.

--- FloydTriangle.java
+++ FloydTriangle.java
@@ -1,21 +1,15 @@
-import java.util.Scanner;
-
 public class FloydTriangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int n = 5;
         int count = 1;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(count);
-                if (j < i) System.out.print(" ");
+                System.out.print(count + (j < i ? " " : ""));
                 count++;
             }
             System.out.println();
         }
-
-        sc.close();
     }
 }
